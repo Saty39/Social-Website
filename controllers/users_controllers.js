beece49486c8c465753f231e3f3c21aca932d465
Satyam Raj <sattyrajpoot39@gmail.com>
2020-01-26 23:26:50 +0530

    updatig sigup page

---
+++ controllers/users_controllers.js
@@ -0,0 +1,58 @@
+
+const User=require('../models/user');
+
+module.exports.profile=function(req,res){
+    res.render('user_profile',{
+        title:"Profile"
+    });
+}
+
+//render the sign up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    })
+}
+
+//render the sign in page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codeial | Sign In"
+    })
+}
+
+//get the sign up data
+module.exports.create =function(req,res){
+   if(req.body.password!=req.body.confirm_password)
+   {
+       return res.redirect('back');
+   }
+    
+
+   User.findOne({email: req.body.email},function(err,user){
+       if(err){
+           console.log('error in finding user in signing up'); 
+           return;
+        }
+       //user not found
+       if(!user){
+           User.create(req.body,function(err,user){
+               if(err){
+                   console.log('error in creating user while signing up'); 
+                   return;}
+              //send created user to sign-in page
+                return res.redirect('/users/sign-in');
+            
+           });
+      }
+      else{
+          return res.redirect('back');
+      }   
+       
+   })
+}
+
+//sign in and create a session for the user
+module.exports.createSession =function(req,res){
+    //ToDo Later
+}
--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express=require('express');
-
+const cookieParser=require('cookie-parser');
 const app=express();
 
 const port=3000;
+
+const expessLayouts= require('express-ejs-layouts');
+
+const db=require('./config/mongoose');
+//reading through the post requests
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+
+app.use(expessLayouts);
+
+//using expressLayouts to insert css and script tag in proper place in ejs file
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+//accesing route middleware
+app.use('/',require('./routes'));
+
+//setting up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
 app.listen(port,function(err){
