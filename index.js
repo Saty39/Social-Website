767cf145a17dfd08cf09cb03eae3135977fd1052
Satyam Raj <sattyrajpoot39@gmail.com>
2020-02-03 08:25:00 +0530

    delete comment and posts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const cookieParser=require('cookie-parser');
 const app=express();
 
-const port=1500;
+const port=4500;
 
 const expessLayouts= require('express-ejs-layouts');
 
